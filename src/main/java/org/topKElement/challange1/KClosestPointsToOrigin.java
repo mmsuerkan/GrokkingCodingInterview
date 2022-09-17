e6ec06f26b2e52ca3dde0b92f93fb23b2bd95ce7
msuerkan <msuerkan@partner.innova.com.tr>
2022-09-17 16:34:00 +0300

    K Closest Point To Origin

---
+++ src/main/java/org/topKElement/challange1/KClosestPointsToOrigin.java
@@ -0,0 +1,47 @@
+package org.topKElement.challange1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int distFromOrigin() {
+        // ignoring sqrt
+        return (x * x) + (y * y);
+    }
+}
+public class KClosestPointsToOrigin {
+
+    public static List<Point> findClosestPoints(Point[] points, int k) {
+        PriorityQueue<Point> maxHeap = new PriorityQueue<Point>((n1, n2) -> n2.distFromOrigin() - n1.distFromOrigin());
+        ArrayList<Point> result = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            maxHeap.add(points[i]);
+        }
+        for (int i = k; i < points.length; i++) {
+
+            if(points[i].distFromOrigin() < maxHeap.peek().distFromOrigin()){
+                maxHeap.poll();
+                maxHeap.add(points[i]);
+            }
+        }
+        return new ArrayList<>(maxHeap);
+    }
+
+    public static void main(String[] args) {
+        Point[] points = new Point[] { new Point(1, 3), new Point(3, 4), new Point(2, -1) };
+        List<Point> result = KClosestPointsToOrigin.findClosestPoints(points, 2);
+        System.out.print("Here are the k points closest the origin: ");
+        for (Point p : result)
+            System.out.print("[" + p.x + " , " + p.y + "] ");
+    }
+}
