44a185d7041641081b5b3cd8c184cf5030592170
msuerkan <msuerkan@partner.innova.com.tr>
2022-10-29 17:41:40 +0300

    Tree Diameter

---
+++ src/main/java/org/dfs/challenge7/TreeDiameter.java
@@ -0,0 +1,37 @@
+package org.dfs.challenge7;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+};
+
+public class TreeDiameter {
+    private static int findDiameter(TreeNode root) {
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        System.out.println("Tree Diameter: " + TreeDiameter.findDiameter(root));
+        root.left.left = null;
+        root.right.left.left = new TreeNode(7);
+        root.right.left.right = new TreeNode(8);
+        root.right.right.left = new TreeNode(9);
+        root.right.left.right.left = new TreeNode(10);
+        root.right.right.left.left = new TreeNode(11);
+        System.out.println("Tree Diameter: " + TreeDiameter.findDiameter(root));
+    }
+
+
+
+}
--- src/main/java/org/subsets/challenge1/Subsets.java
+++ src/main/java/org/subsets/challenge1/Subsets.java
@@ -2,6 +2,7 @@ package org.subsets.challenge1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Subsets {
 
@@ -16,7 +17,7 @@ public class Subsets {
             for (int i = 0; i < n; i++) {
                 List<Integer> set = new ArrayList<>(subsets.get(i));
                 set.add(currentNumber);
-                subsets.add(set);
+                    subsets.add(set);r
             }
         }
         return subsets;
@@ -25,9 +26,16 @@ public class Subsets {
     public static void main(String[] args) {
 //        List<List<Integer>> result = Subsets.findSubsets(new int[]{1, 3});
 //        System.out.println("Here is the list of subsets: " + result);
-
+        int sum = 0;
         List<List<Integer>> result = Subsets.findSubsets(new int[]{1, 5, 3});
-        System.out.println("Here is the list of subsets: " + result);
+        Stream<List<Integer>> listStream = result.stream().filter(list -> list.size() % 2 != 0);
+        for (List<Integer> list : listStream.toArray(List[]::new)) {
+
+            for (Integer integer : list) {
+                sum += integer;
+            }
+        }
+        System.out.println(sum);
     }
 
 }
